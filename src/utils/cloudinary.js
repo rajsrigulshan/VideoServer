55ae8604b696bffa3255a217811d3f2b23d68e15
RAJ_SRI_GULSHAN <rajgulshan20022000@gmail.com>
2024-10-08 14:11:13 +0530

    project setup completed

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,33 @@
+import {v2 as cloudinary} from "cloudinary";
+import fs from "fs";
+
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+
+const uploadOnCloudinary= async (localFilePath)=>{
+    try {
+            if(!localFilePath) return null;
+
+            //uploads the file to the cloudianary
+            const response=await cloudinary.uploader.upload(localFilePath,{
+                resource_type:"auto"
+            })
+
+            console.log("file uploaded on cloudniary. ",response.url);
+            return response;
+    } catch (error) {
+        fs.unlinkSync(localFilePath); //removes the locally stored file
+        console.log(error);
+        return null;
+        
+    }
+
+
+};
+
+export {uploadOnCloudinary}
